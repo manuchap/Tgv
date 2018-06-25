9e2d675ac323d6cc04b80b6b9de2d4b36b315a11
manuchap <manuchap@gmail.com>
2018-06-25 20:43:29 +0200

    js rad

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,41 +1,10 @@
-function toggleConf() {
-  document.getElementById("conf").onchange = function() {
-    var x = document.getElementsByClassName("conf");
-    if (document.getElementById("conf").checked === true) {
-      var i;
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "block";
-      }
-    } else for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-      }
-  };
-}
-
-function toggleGemf() {
-  document.getElementById("gemf").onchange = function() {
-    var x = document.getElementsByClassName("gemf");
-    if (document.getElementById("gemf").checked === true) {
-      var i;
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "block";
-      }
-    } else for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-      }
-  };
-}
-
-function toggleSyst() {
-  document.getElementById("syst").onchange = function() {
-    var x = document.getElementsByClassName("syst");
-    if (document.getElementById("syst").checked === true) {
-      var i;
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "block";
-      }
-    } else for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-      }
-  };
+function toggleConf() { toggleGeneric("conf"); }
+function toggleGemf() { toggleGeneric("gemf"); }
+function toggleSyst() { toggleGeneric("syst"); }
+function toggleGeneric(element) {
+  var e, n = document.getElementsByClassName(element);
+  if (!0 === document.getElementById(element).checked)
+    for (e = 0; e < n.length; e++) n[e].style.display = "block";
+  else
+    for (e = 0; e < n.length; e++) n[e].style.display = "none";
 }
