1b2a2d0d75d654bab69aea5ded2e706af7178a4e
manuchap <manuchap@gmail.com>
2018-07-08 14:18:59 +0200

    misc

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -23,14 +23,14 @@ function toggleGeneric(element) {
   var e, n = document.getElementsByClassName(element);
   if (!0 === document.getElementById(element).checked)
     for (e = 0; e < n.length; e++) n[e].style.display = "block";
-  else
-    for (e = 0; e < n.length; e++) n[e].style.display = "none";
-}
+      else
+        for (e = 0; e < n.length; e++) n[e].style.display = "none";
+      }
 
 // Rendertime
 var renderStart = new Date().getTime();
 window.onload=function() {
-var elapsed = new Date().getTime()-renderStart;
-document.getElementById("speed").innerHTML = elapsed;
+  var elapsed = new Date().getTime()-renderStart;
+  document.getElementById("speed").innerHTML = elapsed;
 };
 
